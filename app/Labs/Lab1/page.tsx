4228d621a4853d3d50795fa67567a25d66bbc40a
Bridget Leary <beleary25@gmail.com>
2025-09-14 19:19:06 -0400

    rerouted labs landing page

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h1>Labs</h1>
+            <h2>Bridget Leary - SEC 05</h2>
+            <ul>
+                <li>
+                    <Link href="/Labs/Lab1" id="wd-lab1-link">
+                        Lab 1: HTML Examples </Link>
+                </li>
+                <li>
+                    <Link href="/Labs/Lab2" id="wd-lab2-link">
+                        Lab 2: CSS Basics </Link>
+                </li>
+                <li>
+                    <Link href="/Labs/Lab3" id="wd-lab3-link">
+                        Lab 3: JavaScript Fundamentals </Link>
+                </li>
+                <li>
+                    <Link href="/" id="wd-kambaz-link">
+                        Kambaz </Link>
+                </li>
+            </ul>
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
 
