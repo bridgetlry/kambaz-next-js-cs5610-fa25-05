60f4464b6f0484f6f2f55ab3dbfc5b8a71f50593
Bridget Leary <beleary25@gmail.com>
2025-09-14 16:42:57 -0400

    fixed cid bug

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 export default async function CoursesLayout(
   { children, params }: Readonly<{ children: ReactNode;
-    params: Promise<{ id: string }> }>) {
- const { id: cid } = await params;
+    params: Promise<{ cid: string }> }>) {
+ const { cid } = await params;
  return (
    <div id="wd-courses">
      <h2>Courses {cid}</h2>
