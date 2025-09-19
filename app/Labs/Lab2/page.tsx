05cdfd95663a3ad3ec7d23d1d62d25b441915174
Bridget Leary <beleary25@gmail.com>
2025-09-18 20:52:01 -0400

    2.1 from book

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,8 +1,43 @@
+import "./index.css";
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import Selectors from "./Selectors";
+
+
 export default function Lab2() {
-    return (
-      <div id="wd-lab2">
-        <h2>Lab 2</h2>
-        <h3>Intro to CSS</h3>
-      </div>
-  );}
-  
+  return (
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      <p style={{ backgroundColor: "blue",
+                  color: "white" }}>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+      <Selectors />
+      <ForegroundColors />
+      <BackgroundColors />
+      <Borders />
+      <Padding />
+      <Margins />
+      <Corners />
+      <Dimensions />
+      <Positions />
+      <Zindex />
+      <Float />
+      <GridLayout />
+      <Flex />
+    </div>
+);}
