da82470b091a04546a144e6e0e29b090cec69443
Bridget Leary <beleary25@gmail.com>
2025-11-12 19:54:09 -0500

    minor changes

--- app/Labs/Lab5/WorkingWithArrays.tsx
+++ app/Labs/Lab5/WorkingWithArrays.tsx
@@ -45,8 +45,6 @@ export default function WorkingWithArrays() {
             <h3>Updating an Item in an Array</h3>
             <a href={`${API}/${todo.id}/title/${todo.title}`} className="btn btn-primary float-end">
                 Update Todo</a>
-            <FormControl defaultValue={todo.id} className="w-25 float-start me-2"
-                onChange={(e) => setTodo({ ...todo, id: e.target.value })} />
             <FormControl defaultValue={todo.title} className="w-50 float-start"
                 onChange={(e) => setTodo({ ...todo, title: e.target.value })} />
             <br /><br />
@@ -59,8 +57,6 @@ export default function WorkingWithArrays() {
             
             <a href={`${API}/${todo.id}/completed/${todo.completed}`} className="btn btn-primary">
                 Update Completed</a>
-            <FormControl defaultValue={todo.id} className="w-25 float-start me-2"
-                onChange={(e) => setTodo({ ...todo, id: e.target.value })} />
             <FormCheck type="checkbox" defaultChecked={todo.completed} className="w-50 float-start"
                 onChange={(e) => setTodo({ ...todo, completed: e.target.checked })} />
 
